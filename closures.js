391ab0a0c993704b39c9a213bc76ded931e0e340
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-17 15:29:25 -0600

    done

--- closures.js
+++ closures.js
@@ -210,15 +210,13 @@ function secretNumber() {
 function timeOutCounter() {
 
   for (var i = 0; i <= 5; i++) {
-
     closure = function(){
       let x = i;
       setTimeout(function() {
-        
         console.log(x);
       }, x * 1000);
+    }
+    closure();
   }
-  closure();
-}
 }
 timeOutCounter();
